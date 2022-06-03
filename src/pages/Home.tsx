8ad4dd8239f9649e335f8c600e814f25a50bd53c
Nikolai Rodionov <allanger@zohomail.com>
2022-06-03 21:11:38 +0200

    some update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import PageBody from '../components/PageBody';
-import { Colors } from '../consts';
-import { Helmet } from 'react-helmet';
+import {Colors} from '../consts';
+import {Helmet} from 'react-helmet';
 import background from "../assets/me.jpg";
 import logo from "../assets/logo.png";
 const style = {
@@ -18,6 +18,7 @@ const contentStyle = {
 const rel: "relative" = "relative";
 
 const subContentStyle = {
+  margin: "5%",
   flex: 1,
 }
 
@@ -49,8 +50,6 @@ const content = (
     </div>
     <div style={contentStyle}>
       <div style={subContentStyle}>
-      </div>
-      <div style={subContentStyle}>
         <h1>Well, hello there!</h1>
         <p>I am allanger: musician, sound engineer, beatmaker and producer.</p>
         <p>Currently I'm living in Duesseldorf and looking for local artists to work with.</p>
@@ -62,10 +61,25 @@ const content = (
         <p>That includes limitless free revisions. If you like the result, you will receive the mixed version abd couple of mastered versions <br />I'm not a mastering guru but it will give you an understanding how your song can sound if it's loud enough. Or maybe you will like the mastered version so much <br />that you won't have look for a mastering engineer. Also I will write down everything I'm doing and that means that if you come to me one more time<br /> I will know already which effects fit your voice, or which frequencies should be cleaned up.</p>
         <h3>I can write music for you</h3>
         <p>That includes beats, arrangements, soundtracks, etc. Just send describe what you want, or send references. Currently I don't have any instruments except a midi keyboard, a laptop and a ukulele, so I'm doing mostly the electronic stuff. But I can write notes for acoustic instrument as well, or find out how to record them, if you wan't it very badly.</p>
-
-        
       </div>
-
+      <div style={subContentStyle}>
+        <form>
+          <label>
+            What do you want?
+          <select>
+            <option value="Mixing">Grapefruit</option>
+            <option value="Fast Mixing">Lime</option>
+            <option selected value="Beat from the stock">Coconut</option>
+            <option value="Custom beat/arrangement">Mango</option>
+          </select>
+          </label>
+          <label>
+            Name:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
     <div>
 
